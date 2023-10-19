e842370a3272907bb2ded259209d7feeda627267
David Berry <dberry3755@gmail.com>
2023-10-19 11:28:55 -0400

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import { CeoList } from "./components/CeoList";
+import "./App.css";
+
+
+function App() {
+  return (
+    <>
+      <CeoList />
+    </>
+  );
+}
+
+export default App;
